d35ba3a6ebdf95ee8ea9623885ff0d6ee7a2e826
antonio-fiorentino <antoniofiorentino16@gmail.com>
2020-12-22 13:03:11 +0100

    Esercizio

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ console.log('Il nome del Burger è: ' + nomeBurger);
 var containerIngredienti = document.getElementsByClassName('ingredienti')[0];
 var checkList = containerIngredienti.getElementsByTagName('input');
 var totale = document.getElementById('prezzo');
+var coupon = document.getElementsByClassName('coupon');
+
+var checkedIngredient = 0;
+var cost;
+
+document.getElementById('button-submit').addEventListener("click", function(){
+  checkedIngredient = 0;
+  cost = burgerCost;
+  for(var i=0 ; i < checkList.length; i++)
+  if (checkList[i].checked) {
+    checkedIngredient++
+  }
+  
+})
