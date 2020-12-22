64b5e8d9be7405af9b984cee25562c36919f9e51
antonio-fiorentino <antoniofiorentino16@gmail.com>
2020-12-22 13:21:12 +0100

    Esercizio

--- js/script.js
+++ js/script.js
@@ -1,26 +1,6 @@
 var minIngredienti = 2;
-var burgerCost = 50;
-var discount = 0.2;
-var coupons = ['12GD54PEOR','HF4hydsiu8','ter7EPD5sa']
+var burgerBaseCosto = 50;
 
-
-var nomeBurger = document.getElementById('nome');
-console.log('Il nome del Burger è: ' + nomeBurger);
-
+var nomeBurger = document.getElementById('nome-burger');
 var containerIngredienti = document.getElementsByClassName('ingredienti')[0];
-var checkList = containerIngredienti.getElementsByTagName('input');
-var totale = document.getElementById('prezzo');
-var coupon = document.getElementsByClassName('coupon');
-
-var checkedIngredient = 0;
-var cost;
-
-document.getElementById('button-submit').addEventListener("click", function(){
-  checkedIngredient = 0;
-  cost = burgerCost;
-  for(var i=0 ; i < checkList.length; i++)
-  if (checkList[i].checked) {
-    checkedIngredient++
-  }
-  
-})
+var checkboxList = containerIngredienti.getElementsByTagName('input');
